c2a309e3f25353ab7341788bd5f4fd54f73c17c8
tartor0 <gaaditartor160@gmail.com>
2025-10-24 23:42:08 +0100

    events2 added

---
+++ src/main/java/com/example/dungeoncrawler/EventRect.java
@@ -0,0 +1,9 @@
+package com.example.dungeoncrawler;
+
+import java.awt.*;
+
+public class EventRect extends Rectangle {
+
+    int eventRectDefaultX, eventRectDefaultY;
+    boolean eventDone = false;
+}
